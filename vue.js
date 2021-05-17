53e96f2e5edfcb67e8652896265fc4e514696308
Aleksander Witocha <alekwit@gmail.com>
2021-05-17 12:55:02 +0200

    UI template start

---
+++ vue.js
@@ -0,0 +1,11 @@
+var navbar = new Vue({ 
+    el: '#navbar',
+    data: {
+        newFile: 'New File',
+        uploadFile: 'Upload File',
+        myFiles: 'My Files',
+        username: 'Username',
+        options: 'Options'
+    }
+});
+
